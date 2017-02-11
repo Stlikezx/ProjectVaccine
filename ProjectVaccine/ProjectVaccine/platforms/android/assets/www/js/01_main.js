6c849058df55442f7f4fbe52f25a00f467f16332
Stlikezx <stlikez@hotmail.com>
2017-02-12 00:40:33 +0700

    Fix google function 80 percent

--- ProjectVaccine/ProjectVaccine/platforms/android/assets/www/js/01_main.js
+++ ProjectVaccine/ProjectVaccine/platforms/android/assets/www/js/01_main.js
@@ -1,4 +1,4 @@
-var mainApp = angular.module("mainApp", ["ionic", "firebase"]);
+var mainApp = angular.module('mainApp', ['ionic', 'firebase']);
 
 var config = {
     apiKey: "AIzaSyCg3zxVD2ykqAhypLZe7rZ4ZjtfnnpEL_k",
@@ -14,24 +14,51 @@ provider.addScope('https://www.googleapis.com/auth/plus.login');
 
 
 
-mainApp.controller("googleCtrl", ["$scope", "$firebaseAuth",
-     function ($scope, $firebaseAuth, myservice) {
+mainApp.controller('googleCtrl', ['$scope', '$firebaseAuth', 'dataShare',
+     function ($scope, $firebaseAuth, dataShare) {
          $scope.login = function () {
              firebase.auth().signInWithPopup(provider).then(function (result) {
-                 $scope.user = result.user.providerData[0].email;
+                 $scope.email = result.user.providerData[0].email;
                  $scope.$apply();
+                 dataShare.sendData($scope.email);
              }).catch(function (error) {
-                 // Handle Errors here.
+
                  var errorCode = error.code;
                  var errorMessage = error.message;
-                 // The email of the user's account used.
                  var email = error.email;
-                 // The firebase.auth.AuthCredential type that was used.
                  var credential = error.credential;
-                 // ...
+                 $scope.error = errorMessage;
+                 $scope.$apply();
+                 dataShare.sendData($scope.error);
              });
 
-         }
+         };
 
      }
 ]);
+
+
+mainApp.controller('googleCtrl2', ['$scope', 'dataShare',
+    function ($scope, dataShare) {
+        $scope.text = '';
+        $scope.$on('data_shared', function () {
+            var text = dataShare.getData();
+            console.log(text);
+            $scope.text = text;
+            $scope.$apply();
+        });
+    }
+]);
+mainApp.factory('dataShare', function ($rootScope) {
+    var service = {};
+    service.data = false;
+    service.sendData = function (data) {
+        console.log(data);
+        this.data = data;
+        $rootScope.$broadcast('data_shared');
+    };
+    service.getData = function () {
+        return this.data;
+    };
+    return service;
+});
